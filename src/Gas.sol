8d80fc1091359b7c1110471191c71165d68209a1
iorulezz <iorulezz@gmail.com>
2023-05-07 18:42:35 +0200

    refactor white transfer logic

--- src/Gas.sol
+++ src/Gas.sol
@@ -4,9 +4,8 @@ pragma solidity 0.8.19;
 contract GasContract {
     uint256 private immutable totalSupply; // = 0; // cannot be updated
     mapping(address => uint256) public balances;
-    uint256 public constant tradePercent = 12;
     address public contractOwner;
-    mapping(address => uint256) public whitelist;
+    mapping(address => uint8) public whitelist;
     address[] public administrators; // reconsider if removing 5 here makes it worse Ant0
     mapping(address => uint256) public whiteListStruct;
 
@@ -20,24 +19,6 @@ contract GasContract {
             }
         }
         return false;
-    }
-
-    modifier checkIfWhiteListed(address sender) {
-        address senderOfTx = msg.sender;
-        require(
-            senderOfTx == sender,
-            "Gas Contract CheckIfWhiteListed modifier : revert happened because the originator of the transaction was not the sender"
-        );
-        uint256 usersTier = whitelist[senderOfTx];
-        require(
-            usersTier > 0,
-            "Gas Contract CheckIfWhiteListed modifier : revert happened because the user is not whitelisted"
-        );
-        require(
-            usersTier < 4,
-            "Gas Contract CheckIfWhiteListed modifier : revert happened because the user's tier is incorrect, it cannot be over 4 as the only tier we have are: 1, 2, 3; therfore 4 is an invalid tier for the whitlist of this contract. make sure whitlist tiers were set correctly"
-        );
-        _;
     }
 
     event WhiteListTransfer(address indexed);
@@ -60,28 +41,17 @@ contract GasContract {
         string calldata _name
     ) public returns (bool status_) {
         address senderOfTx = msg.sender;
-        require(
-            balances[senderOfTx] >= _amount,
-            "Gas Contract - Transfer function - Sender has insufficient Balance"
-        );
-        require(
-            bytes(_name).length < 9,
-            "Gas Contract - Transfer function -  The recipient name is too long, there is a max length of 8 characters"
-        );
+        require(balances[senderOfTx] >= _amount);
         balances[senderOfTx] -= _amount;
         balances[_recipient] += _amount;
-        bool[] memory status = new bool[](tradePercent);
-        for (uint256 i = 0; i < tradePercent; i++) {
-            status[i] = true;
-        }
-        return (status[0] == true);
+        return true;
     }
 
     function addToWhitelist(address _userAddrs, uint256 _tier) public {
         address senderOfTx = msg.sender;
         require(senderOfTx == contractOwner || checkForAdmin(senderOfTx));
         require(_tier < 255);
-        whitelist[_userAddrs] = _tier;
+        whitelist[_userAddrs] = uint8(_tier);
         if (_tier > 3) {
             whitelist[_userAddrs] = 3;
         }
@@ -91,23 +61,12 @@ contract GasContract {
     function whiteTransfer(
         address _recipient,
         uint256 _amount
-    ) public checkIfWhiteListed(msg.sender) {
+    ) public {
         address senderOfTx = msg.sender;
         whiteListStruct[senderOfTx] = _amount;
-
-        require(
-            balances[senderOfTx] >= _amount,
-            "Gas Contract - whiteTransfers function - Sender has insufficient Balance"
-        );
-        require(
-            _amount > 3,
-            "Gas Contract - whiteTransfers function - amount to send have to be bigger than 3"
-        );
-        balances[senderOfTx] -= _amount;
-        balances[_recipient] += _amount;
-        balances[senderOfTx] += whitelist[senderOfTx];
-        balances[_recipient] -= whitelist[senderOfTx];
-
+        uint8 usersTier = whitelist[senderOfTx];
+        balances[senderOfTx] -= _amount - usersTier;
+        balances[_recipient] += _amount - usersTier;
         emit WhiteListTransfer(_recipient);
     }
 
