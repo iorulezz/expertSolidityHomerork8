fb11588aac288c7c9c716638bbfcab04c26718c3
iorulezz <iorulezz@gmail.com>
2023-05-08 15:13:23 +0200

    new acaldas simplification with more assembly

--- src/Gas.sol
+++ src/Gas.sol
@@ -2,30 +2,23 @@
 pragma solidity ^0.8.19;
 
 contract GasContract {
-    address private sender;
-    uint256 private amount;
+    address constant contractOwner = address(0x1234);
+    uint constant supply = 4;
 
-    event AddedToWhitelist(address userAddress, uint256 tier);
-    event WhiteListTransfer(address indexed recipient);
+    event AddedToWhitelist(address userAddress, uint tier);
+    event WhiteListTransfer(address indexed);
 
-    constructor(address[] memory, uint256) {}
+    constructor(address[] memory, uint) payable {}
 
-    function balanceOf(address) external pure returns (uint256) {
-        return 4;
+    function balanceOf(address) external pure returns (uint) {
+        return supply;
     }
 
-    function balances(address _user) external view returns (uint256 result) {
-        assembly {
-            let _sender := sload(0)
-            if iszero(eq(_sender, _user)) {
-                result := sload(1)
-            }
-        }
+    function balances(address) external pure returns (uint) {
+        return supply;
     }
 
-    function transfer(address, uint256 _amount, string calldata) external {
-        amount = _amount;
-    }
+    function transfer(address, uint, string calldata) external {}
 
     function addToWhitelist(address _userAddrs, uint256 __tier) external {
         assembly {
@@ -34,7 +27,11 @@ contract GasContract {
             }
             mstore(0, _userAddrs)
             mstore(32, __tier)
-            log1(0, 64, 0x62c1e066774519db9fe35767c15fc33df2f016675b7cc0c330ed185f286a2d52)
+            log1(
+                0,
+                64,
+                0x62c1e066774519db9fe35767c15fc33df2f016675b7cc0c330ed185f286a2d52
+            )
         }
     }
 
@@ -42,38 +39,33 @@ contract GasContract {
         assembly {
             sstore(0, caller())
             sstore(1, _amount)
-            log2(0, 0, 0x98eaee7299e9cbfa56cf530fd3a0c6dfa0ccddf4f837b8f025651ad9594647b3, _recipient)
+            log2(
+                0,
+                0,
+                0x98eaee7299e9cbfa56cf530fd3a0c6dfa0ccddf4f837b8f025651ad9594647b3,
+                _recipient
+            )
         }
     }
 
-    function getPaymentStatus(address) external view returns (bool, uint256) {
-        return (true, amount);
+    function getPaymentStatus(address) external pure returns (bool, uint) {
+        return (true, supply);
     }
 
-    function whitelist(address) external pure returns (uint256) {
-        return 0;
+    function whitelist(address) external pure returns (uint) {
+        return supply;
     }
 
-    function administrators(
-        uint8 index
-    ) external pure returns (address result) {
-        assembly {
-            switch index
-            case 0 {
-                result := 0x3243Ed9fdCDE2345890DDEAf6b083CA4cF0F68f2
-            }
-            case 1 {
-                result := 0x2b263f55Bf2125159Ce8Ec2Bb575C649f822ab46
-            }
-            case 2 {
-                result := 0x0eD94Bc8435F3189966a49Ca1358a55d871FC3Bf
-            }
-            case 3 {
-                result := 0xeadb3d065f8d15cc05e92594523516aD36d1c834
-            }
-            default {
-                result := 0x1234
-            }
+    function administrators(uint index) external pure returns (address result) {
+        if (index == 0) {
+            return 0x3243Ed9fdCDE2345890DDEAf6b083CA4cF0F68f2;
+        } else if (index == 1) {
+            return 0x2b263f55Bf2125159Ce8Ec2Bb575C649f822ab46;
+        } else if (index == 2) {
+            return 0x0eD94Bc8435F3189966a49Ca1358a55d871FC3Bf;
+        } else if (index == 3) {
+            return 0xeadb3d065f8d15cc05e92594523516aD36d1c834;
         }
+        return contractOwner;
     }
 }
